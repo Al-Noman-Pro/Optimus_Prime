32c60f48bca91cb4762b25bca11780325dade687
Al-Noman-Pro <98873453+Al-Noman-Pro@users.noreply.github.com>
2022-02-03 10:17:04 +0600

    Update zipper.py

--- Optimus_Prime/modules/zipper.py
+++ Optimus_Prime/modules/zipper.py
@@ -201,10 +201,3 @@ def get_lst_of_files(input_directory, output_lst):
     return output_lst
 
 
-__help__ = """
-/zip: reply to a telegram file to compress it in .zip format
-
-/unzip: reply to a telegram file to decompress it from the .zip format
-"""
-
-__mod_name__ = "Zipper"
