cf6a8cad4b85bef2413162d1829e37520a1d5c6f
Al-Noman-Pro <98873453+Al-Noman-Pro@users.noreply.github.com>
2022-02-03 10:19:24 +0600

    Update more_tools.py

--- Optimus_Prime/modules/more_tools.py
+++ Optimus_Prime/modules/more_tools.py
@@ -2,21 +2,25 @@ __help__ = """
 
 *Get Fake details from fakenamegenerator.com*                             
 /fakeinfo : returns fake information.
-"""
-*GPS:*
+
+*GPS*
 /gps <Place> : Show Location on a map.
  
-*Blue Text Cleaner:*
+*Blue Text Cleaner*
 /cleanblue on : Turn bluetext cleaner on.
 /cleanblue off : Turn bluetext cleaner off.
 
-*Grammer:*
+*Grammer*
 /t <reply> : Show grammer corrected text.
  
-*Style Text:*
+*Style Text*
 /weebify : Weebify Text.
 /square : square Text.
 /blue : Blues text.
+
+*Zip - Unzip*
+/zip: reply to a telegram file to compress it in .zip format.
+/unzip: reply to a telegram file to decompress it from the .zip format.
 
 """
 __mod_name__ = "More Tools"
