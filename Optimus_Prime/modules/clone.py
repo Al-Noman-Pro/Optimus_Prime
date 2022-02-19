ec8b9128f329d0c4cb72cef14ec5c4f8fd294d30
Al-Noman-Pro <98873453+Al-Noman-Pro@users.noreply.github.com>
2022-02-19 07:51:58 +0600

    Bug Fixed

--- Optimus_Prime/modules/clone.py
+++ Optimus_Prime/modules/clone.py
@@ -92,5 +92,5 @@ def cloneNode(update, context):
     else:
         sendMessage('Send Gdrive or gdtot link along with command or by replying to the link by command', context.bot, update)
 
-clone_handler = CommandHandler("clone", cloneNode, filters=CustomFilters.authorized_chat, run_async=True)
+clone_handler = CommandHandler("clone", cloneNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user | CustomFilters.owner_filter, run_async=True)
 dispatcher.add_handler(clone_handler)
